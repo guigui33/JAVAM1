3e1e1d30f323d129d61a47917e5038d94ce21e16
guigui33 <guillaumecantal@gmail.com>
2016-01-19 11:39:25 +0100

    client + serveur

--- mavenproject1/src/main/java/com/strim1/mavenproject1/Main.java
+++ mavenproject1/src/main/java/com/strim1/mavenproject1/Main.java
@@ -10,7 +10,8 @@ public class Main{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        new Accueil(1324);
+        System.out.println("debut programme");
+        new Accueil(1324).fonctionnementService();
     }
     
 }
