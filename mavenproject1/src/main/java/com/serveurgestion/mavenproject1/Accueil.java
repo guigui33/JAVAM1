788a27b0d56ece550716dea3179c73d900065c81
guigui33 <guillaumecantal@gmail.com>
2016-01-27 15:54:42 +0100

    serveur gestion: a refaire

--- mavenproject1/src/main/java/com/serveurgestion/mavenproject1/Accueil.java
+++ mavenproject1/src/main/java/com/serveurgestion/mavenproject1/Accueil.java
@@ -20,23 +20,24 @@ import java.util.logging.Logger;
  * renvoie le client sur un thread qui va gerer les demandes des clients
  */
 public class Accueil {
-    private final Bdd bdd;
     private Socket service; //socket de service 
-    private ServerSocket ecoute; 
+
+    private ServerSocket socketClient; 
+    private ServerSocket socketServeur;
+    private int portClient=50003;
+    private int portServeur=50004;
     
     /**
      * Construteur Accueil 
-     * @param port 
-     *            le port du service 
+     * 
      */
-    public Accueil(int port){
+    public Accueil(){
         try {
-            ecoute=new ServerSocket(port);
+            socketClient=new ServerSocket(portClient);
+            socketServeur=new ServerSocket(portServeur);
         } catch (IOException ex) {
             Logger.getLogger(Accueil.class.getName()).log(Level.SEVERE, null, ex);
         }
-        bdd=new Bdd();
-        bdd.connexion();
     }
         
     /**
