257f05a2c5ac8b747603aaf52a488d160c57c721
guigui33 <guillaumecantal@gmail.com>
2016-02-01 23:52:52 +0100

    methodes et mots cles des requêtes client pour serveur Gestion

--- mavenproject1/src/main/java/com/serveurgestion/mavenproject1/TraitementDemande.java
+++ mavenproject1/src/main/java/com/serveurgestion/mavenproject1/TraitementDemande.java
@@ -1,5 +1,6 @@
 package com.serveurgestion.mavenproject1;
 
+import com.strim1.mavenproject1.ServicePostal;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -22,14 +23,32 @@ class TraitementDemande {
         String [] decoupageRequete;
         decoupageRequete=demandeClient.split("#");
         //en fonction du mot cle on redirige vers le service correspondant
-        /* switch(decoupageRequete[0]){
-                
-        default:
-        return "requête inconnue, Usage: motclé#id#demande";
-        }*/
-        return null;
+        switch(decoupageRequete[0]){
+            case "RECHERCHER":
+                return rechercher(decoupageRequete);
+            case "VISITER":
+                return visiter(decoupageRequete);
+            case "MODIFIERUTILISATEUR":
+                return modifierUtilisateur(decoupageRequete);
+            case "AJOUTERDIPLOME":
+                return ajouterDiplome(decoupageRequete);
+            case "AJOUTERCOMP":
+                return ajouterComp(decoupageRequete);
+            case "SUPPCOMPETENCE":
+                return suppCompetence(decoupageRequete);
+            case "SUPPDIPLOME":
+                return suppDiplome(decoupageRequete);
+            case "MODIFVISICOMP":
+                return modifVisiComp(decoupageRequete);
+            case "MODIFVISIDIP":
+                return modifVisiDip(decoupageRequete);
+            case "DECONNEXION":
+                return deconnexion(decoupageRequete);
+            default:
+                return "requête inconnue, Usage: motclé#id#demande";
+        }
     }
-     
+    
     public boolean verificationConnexion(String demandeClient){
         String []demande=demandeClient.split("#");
         if(!demande[0].equals("HELLO")) return false;
@@ -38,27 +57,59 @@ class TraitementDemande {
         }
         try {
             Socket serveurConnexion=new Socket("localhost",50002);
-            PrintStream fluxSortie=null;
-            try {
-                fluxSortie = new PrintStream(serveurConnexion.getOutputStream());
-            } catch (IOException ex) {
-                return false;
-            }
+            ServicePostal servicePostalCo = new ServicePostal(serveurConnexion);
+            
             String requete="VERIFCONNEXION#"+demande[1]+"#"+demande[2];
             System.out.println("requete au serveur de connexion: "+requete);
-            fluxSortie.println(requete);
+            servicePostalCo.emission(requete);
             
-            BufferedReader fluxEntreeSocket =new BufferedReader(new InputStreamReader(serveurConnexion.getInputStream()));
-            String reponse=null;
-            reponse= fluxEntreeSocket.readLine();
+            String reponse= servicePostalCo.reception();
             System.out.println("Reponse du serveur connexion: " + reponse);
-            if(reponse.equals("OK")){
-                return true;
-            }
-            return false;
+            
+            return reponse.equals("OK");//si la reponse est ok ou error
             
         } catch (IOException ex) {
             return false;
         }
+    }
+    
+    private String rechercher(String[] decoupageRequete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String visiter(String[] decoupageRequete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String modifierUtilisateur(String[] decoupageRequete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String ajouterDiplome(String[] decoupageRequete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String ajouterComp(String[] decoupageRequete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String suppCompetence(String[] decoupageRequete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String suppDiplome(String[] decoupageRequete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String modifVisiComp(String[] decoupageRequete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String modifVisiDip(String[] decoupageRequete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private String deconnexion(String[] decoupageRequete) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
