3cfa3ba615d84a72073bd52e632d4fc128ce2f2f
guigui <guigui@guigui-HP>
2016-02-09 21:09:49 +0100

    final

--- mavenproject1/src/main/java/com/strim1/mavenproject1/Bdd.java
+++ mavenproject1/src/main/java/com/strim1/mavenproject1/Bdd.java
@@ -1,20 +1,6 @@
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.strim1.mavenproject1;
 
-/**
- *
- * @author TomGui
- */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 import java.sql.*;
 import java.sql.Date;
 import java.util.logging.Level;
