501e7c25f95cb9b8681d2c8b76fa47d821b648fa
FBadens <floow82@hotmail.fr>
2016-02-25 18:46:09 +0100

    Début Messagerie v2 offline

---
+++ mavenproject1/src/main/java/com/stri/clientmessagerie/AccueilMessage.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stri.clientmessagerie;
+
+/**
+ *
+ * @author AFBD
+ */
+public class AccueilMessage extends Thread{
+    
+    
+    public void run(){
+      while(true)
+        System.out.println("Salut ta fdfze");
+    }
+    
+}
